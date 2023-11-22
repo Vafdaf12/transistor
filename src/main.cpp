95f7d9653cfd62dc834d2ce77ef5a9ad158d1b78
Vincent <vafdaf11@gmail.com>
2023-11-22 22:55:41 +0200

    Added rectangle with view

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include "SFML/Graphics/RectangleShape.hpp"
 #include "SFML/Graphics/RenderWindow.hpp"
+#include "SFML/Graphics/View.hpp"
 #include "SFML/Window/Event.hpp"
 
 #include <iostream>
@@ -7,6 +9,16 @@ int main(int, char**) {
     std::cout << "Hello, World!" << std::endl;
     sf::RenderWindow window({1280, 720}, "Transistor");
 
+    sf::View view;
+    view.setCenter({0, 0});
+    view.setSize({1280, 720});
+    window.setView(view);
+
+    sf::RectangleShape shape;
+    shape.setPosition({0, 0});
+    shape.setSize({100, 100});
+    shape.setFillColor(sf::Color::Yellow);
+
     while(window.isOpen()) {
         for(sf::Event event; window.pollEvent(event);) {
             if(event.type == sf::Event::Closed) {
@@ -14,6 +26,7 @@ int main(int, char**) {
             }
         }
         window.clear();
+        window.draw(shape);
         window.display();
     }
     return 0;
