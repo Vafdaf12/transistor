1084a1e65ee5e73e536d003238eda7f514d3b37e
Vincent <vafdaf11@gmail.com>
2023-12-20 17:54:23 +0200

    Formatting things

--- src/core/Entity.h
+++ src/core/Entity.h
@@ -4,11 +4,11 @@
 #include "SFML/Window/Event.hpp"
 
 namespace core {
-    class Entity {
-    public:
-        virtual ~Entity() {}
-        virtual void onEvent(const sf::RenderWindow&, const sf::Event&) = 0;
-        virtual void update(const sf::RenderWindow&, float dt) = 0;
-        virtual void draw(sf::RenderWindow&) const = 0;
-    };
-}
+class Entity {
+public:
+    virtual ~Entity() {}
+    virtual void onEvent(const sf::RenderWindow&, const sf::Event&) = 0;
+    virtual void update(const sf::RenderWindow&, float dt) = 0;
+    virtual void draw(sf::RenderWindow&) const = 0;
+};
+} // namespace core
