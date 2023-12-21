b36e90d56e4ba49e6afb8333489422739b08738d
Vincent <vafdaf11@gmail.com>
2023-12-21 19:02:25 +0200

    Removed comments that are implemented

--- src/main.cpp
+++ src/main.cpp
@@ -75,54 +75,11 @@ int main(int, char**) {
     }
 
     /*
-    std::cout << sf::Clipboard::getString().toAnsiString() << std::endl;
-    sf::RenderWindow window({1280, 720}, "Transistor");
-    window.setKeyRepeatEnabled(false);
-
-    Assets assets;
-    assets.fonts.load("default", "assets/fonts/Iosevka-Regular.ttc");
-    assets.textures.load("gate_or", "assets/sprites/gate_or.png");
-    assets.textures.load("gate_xor", "assets/sprites/gate_xor.png");
-    assets.textures.load("gate_and", "assets/sprites/gate_and.png");
-    assets.textures.load("gate_not", "assets/sprites/gate_not.png");
-
-    CircuitRegistry registry;
-    registry.add("or_gate", [&assets](const json& j) { return serde::createOr(j, assets); });
-    registry.add("xor_gate", [&assets](const json& j) { return serde::createXor(j, assets); });
-    registry.add("and_gate", [&assets](const json& j) { return serde::createAnd(j, assets); });
-    registry.add("nand_gate", [&assets](const json& j) { return serde::createNand(j, assets); });
-    registry.add("not_gate", [&assets](const json& j) { return serde::createNot(j, assets); });
-
-    DragBoard board;
-
-    sf::View view;
-    sf::Transform x;
-    view.setCenter({0, 0});
-    view.setSize({1280, 720});
 
     GameWorld world;
     world.loadFromFile("assets/world.json", registry);
     CommandLoader loader;
 
-    int circuitCount = 0;
-    SfLayer guiLayer;
-    SfLayer worldLayer;
-
-    sf::Vector2i mouse = sf::Mouse::getPosition();
-
-    ToolState state = NONE;
-
-    std::list<Tool*> tools;
-    tools.push_back(new NavigationTool(view));
-    tools.push_back(new PinConnector(world));
-
-    CircuitDragger* dragger = new CircuitDragger(world, board);
-    tools.push_back(dragger);
-
-    SelectionTool* selector = new SelectionTool(world, board);
-    tools.push_back(selector);
-
-    Circuit* dragBoard = nullptr;
 
     NandCircuit* proto = new NandCircuit("nand", assets);
     BinaryGate* proto2 = new BinaryGate("and", assets, BinaryGate::And);
@@ -163,83 +120,6 @@ int main(int, char**) {
     circuitButtons.back().getShape().setPosition({10, 290});
     circuitButtons.back().getShape().setSize({100, 50});
 
-    // --- CIRCUIT DELETION ---
-    worldLayer.subscribe(sf::Event::KeyReleased, [&](const sf::Event& event) {
-        if (event.key.code != sf::Keyboard::Delete)
-            return false;
-        std::vector<Circuit*> selected = board.getSelection();
-        if (selected.empty()) {
-            std::cout << "Nothing to delete" << std::endl;
-            return false;
-        }
-        for (auto s : selected) {
-            world.removeCircuit(s);
-        }
-
-        board.clearSelection();
-        return true;
-    });
-
-    // --- EVENT LOOP ---
-    while (window.isOpen()) {
-        for (sf::Event e; window.pollEvent(e);) {
-            if (e.type == sf::Event::Closed) {
-                window.close();
-            }
-            window.setView(window.getDefaultView());
-            if (guiLayer.handle(e.type, e))
-                continue;
-
-            window.setView(view);
-
-            bool handled = false;
-            for(auto& btn : circuitButtons) {
-                btn.onEvent(window, e);
-                handled = handled || btn.isActive();
-            }
-            if(handled) {
-                continue;
-            }
-
-            worldLayer.handle(e.type, e);
-
-            window.setView(view);
-            world.onEvent(window, e);
-            for(Tool* tool : tools) {
-                tool->onEvent(window, e);
-            }
-        }
-
-        window.setView(view);
-        world.update(window);
-        for(auto& btn : circuitButtons) {
-            btn.update(window);
-        }
-
-        sf::Vector2i newPos = sf::Mouse::getPosition(window);
-        sf::Vector2f newWorldPos = window.mapPixelToCoords(newPos);
-        for(Tool* tool : tools) {
-            tool->update(window);
-        }
-
-        mouse = newPos;
-
-        window.clear();
-
-        // --- WORLD VIEW ---
-        window.setView(view);
-        world.draw(window);
-
-        for(const Tool* tool : tools) {
-            tool->draw(window);
-        }
-        // --- GUI VIEW ---
-        window.setView(world.getScreenView());
-        for(const auto& btn : circuitButtons) {
-            btn.draw(window);
-        }
-        window.display();
-    }
     world.saveToFile("assets/world.json");
     return 0;
     */
