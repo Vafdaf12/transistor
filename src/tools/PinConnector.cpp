2f34f3aa98fa436517918976adfce082b2394bea
Vincent <vafdaf11@gmail.com>
2024-01-04 00:42:57 +0200

    Fix segfault in pin connector

--- src/tools/PinConnector.cpp
+++ src/tools/PinConnector.cpp
@@ -9,44 +9,33 @@ void PinConnector::update(const sf::RenderWindow& window, float) {
     }
 }
 
-void PinConnector::onEvent(const sf::RenderWindow& window, const sf::Event& event) {
-    if (event.type == sf::Event::MouseButtonPressed) {
-        if (event.mouseButton.button != sf::Mouse::Left) {
+void PinConnector::onEvent(const sf::RenderWindow& window, const sf::Event& e) {
+    sf::Event::MouseButtonEvent event = e.mouseButton;
+    sf::Vector2i eventPos = {event.x, event.y};
+
+    if (e.type == sf::Event::MouseButtonPressed && event.button == sf::Mouse::Left) {
+        Pin* pin = _editor.collidePin(window, eventPos);
+        if (!pin) {
             return;
         }
-        sf::Vector2i mousePos = {event.mouseButton.x, event.mouseButton.y};
-        _firstPin = _editor.collidePin(window, mousePos);
 
-        Wire* wire = _editor.getWire(_firstPin);
-
-        if (_firstPin && _firstPin->getType() == Pin::Input && wire) {
+        Wire* wire = _editor.getWire(pin);
+        if (wire && pin->getType() == Pin::Input) {
             _firstPin = const_cast<Pin*>(wire->getFrom());
             _editor.removeWire(_firstPin, const_cast<Pin*>(wire->getTo()));
-        }
-        if (_firstPin) {
-            sf::Vector2i pos = _firstPin->getScreenSpacePosition(window);
+        } else {
+            _firstPin = pin;
 
-            _vertices[0].position = window.mapPixelToCoords(pos, _editor.getWorldView());
         }
+        sf::Vector2i pos = _firstPin->getScreenSpacePosition(window);
+        _vertices[0].position = window.mapPixelToCoords(pos, _editor.getWorldView());
     }
-    if (event.type == sf::Event::MouseButtonReleased) {
-        if (event.mouseButton.button != sf::Mouse::Left) {
-            return;
+    if (_firstPin && e.type == sf::Event::MouseButtonReleased && event.button == sf::Mouse::Left) {
+        Pin* pin = _editor.collidePin(window, eventPos);
+        if (pin && pin != _firstPin && _firstPin->canConnect(*pin)) {
+            _editor.addWire(_firstPin, pin);
         }
-        sf::Vector2i mousePos = {event.mouseButton.x, event.mouseButton.y};
-        sf::Vector2f worldPos = window.mapPixelToCoords(mousePos, _editor.getWorldView());
-
-        Pin* nextPin = _editor.collidePin(window, mousePos);
-
-        if (!nextPin || nextPin == _firstPin || !_firstPin->canConnect(*nextPin) ||
-            (nextPin->getType() == Pin::Input && _editor.getWire(nextPin))) {
-            _firstPin = nullptr;
-            return;
-        }
-        _editor.addWire(_firstPin, nextPin);
-
         _firstPin = nullptr;
-        return;
     }
 }
 
